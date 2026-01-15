1582e410c915fa1f1c34b052874f32307febf530
loki0b <contato@henriquegarcialeite.com>
2026-01-15 15:03:26 -0300

    feature: adding middleware function for authorization

---
+++ middleware.js
@@ -0,0 +1,14 @@
+const jwt = require("jsonwebtoken");
+
+function middleware(req, res, next) {
+    const token = req.cookies.auth_token;
+
+    if (token === undefined) return res.status(401).json({ error: "Unauthorized" });
+    
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+        if (err) return res.status(401).json({ error: "Unauthorized" });
+
+        req.user = decoded.sub;
+        next();
+    });
+}

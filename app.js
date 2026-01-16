fa88e38a49535adc39b7c2c86db93ccc31179c79
loki0b <contato@henriquegarcialeite.com>
2026-01-16 20:37:19 -0300

    feature: new error middleware

--- app.js
+++ app.js
@@ -9,6 +9,21 @@ app.use(express.json()) // parsing application/json
 app.use(cookieParser())
 app.use("/api/users", usersRouter);
 
+// Middleware to capture errors
+app.use((err, req, res, next) => {
+    console.log(err.stack);
+
+    const status = err.status || 500;
+    const message = err.message || "Internal Server Error";
+
+    res.status(status).json({
+        error: {
+            message: message,
+            status: status
+        }
+    });
+});
+
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
 })

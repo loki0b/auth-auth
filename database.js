42ea7db99fa6ec01b91e4a24cba31c6bb5b6a3f7
loki0b <contato@henriquegarcialeite.com>
2026-01-16 17:17:10 -0300

    feature: async dbget function

--- database.js
+++ database.js
@@ -8,4 +8,13 @@ db.serialize(() => {
             )`)
 })
 
+async function dbGet(sql, params) {
+    return new Promise((resolve, reject) => {
+       db.get(sql, params, (err, row) => {
+            if (err) reject(err);
+            resolve(row);
+       }); 
+    });
+}
+
 module.exports = db
